0529c41c5b87f70ade80da86e81e14748ecbeac3
caiocroci <caiocroci@gmail.com>
2020-06-26 16:00:44 -0300

    Atualização

---
+++ Assets/Scripts/EventLook.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EventLook : MonoBehaviour
+{
+	public GameObject fase3;
+	public GameObject menu;
+	public void ChangeScene()
+	{
+		if (fase3)
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+		}
+
+		if (menu)
+		{
+			SceneManager.LoadScene("MENU");
+		}
+		
+	}
+}
